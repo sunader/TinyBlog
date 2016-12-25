0294e51dc918ed6f49c100f74d09da5c44467c9b
Lei Chen <leichen2003@qq.com>
2016-12-26 00:19:35 +0800

    Add hello world script by python

---
+++ hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Lei Chen
+# @Date:   2016-12-26 00:12:44
+# @Last Modified by:   Lei Chen
+# @Last Modified time: 2016-12-26 00:13:03
+
+print("Hello world!")
